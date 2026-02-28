8aee4ddc8890847bf16434858f8b290d7c37c20d
Kittiphob Subsorn <slaaxx1122@gmail.com>
2026-02-28 14:32:30 +0700

    ทำการเชื่อม database กับ router index.html

--- App.js
+++ App.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const path = require("path")
+const router = require("./routes/index.js")
 const app = express()
 
+require("./config/db.js")
+app.use(express.static(path.join(__dirname, "public")))
+app.use(router)
 
 app.listen(9000, ()=>{
     console.log("ยินดีเข้าสู่เเก๊งทำไมเป็น step#1 test")
